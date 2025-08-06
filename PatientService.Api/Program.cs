262e1d6b8e9d7ecf0f59685db6202e8ef3aec03e
dizzar99 <dizarr99@yandex.ru>
2025-08-06 14:23:21 +0300

    apply migrations on startup

--- PatientService.Api/Program.cs
+++ PatientService.Api/Program.cs
@@ -17,7 +17,7 @@ namespace PatientService.Api
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +39,12 @@ namespace PatientService.Api
 				.AddInfrastructureServices();
 
 			var app = builder.Build();
+
+			using (var scope = app.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+				await db.Database.MigrateAsync();
+			}
 
 			if (app.Environment.IsDevelopment())
 			{
